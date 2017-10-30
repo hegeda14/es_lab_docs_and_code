47d7b00ee8408477908450edd5712d4a5bfb2546
Attila Hegedüs <atheg001@fb4.fh>
2017-10-30 16:35:53 +0100

    Infrared sensors and IR distance measurement

--- projects/es_lab_project/src/main.c
+++ projects/es_lab_project/src/main.c
@@ -1,35 +1,42 @@
+#include <stdio.h>
 #include <stdlib.h>
+#include <inttypes.h>
 #include "dorobo32.h"
 #include "FreeRTOS.h"
 #include "task.h"
 #include "trace.h"
 #include "fft.h"
 #include "wifi.h"
+#include "adc.h"
 
-static void ConfigUART();
+#define IRSENSOR0 DD_PIN_PF10
+#define IRSENSOR1 DD_PIN_PF9
+#define IRDISTANCE0 DA_ADC_CHANNEL0
+#define IRDISTANCE1 DA_ADC_CHANNEL1
+
+static void ConfigWiFi();
 
 static void StatusLED(void *pvParameters);
 static void Switch_0(void *pvParameters);
 static void Switch_1(void *pvParameters);
-static void WiFi(void *pvParameters);
 
-static void IRSens_0(void *pvParameters);
-static void IRSens_1(void *pvParameters);
+static void IRSensors(void *pvParameters);
+static void IRDistance(void *pvParameters);
 
 
 int main()
 {
     dorobo_init();			//Call dorobo_init() function to initialize HAL, Clocks, Timers etc.	
 	
-    ConfigUART();
+    ConfigWiFi();
 
     //xTaskCreate(blinky, "BLINKYTASK", 512, NULL, 2, NULL);	//create blinky task
-    xTaskCreate(Switch_0, "SWITCH0", 128, NULL, 3, NULL);
-    xTaskCreate(Switch_1, "SWITCH1", 128, NULL, 3, NULL);
-    xTaskCreate(IRSens_0, "IRSens0", 128, NULL, 2, NULL);
-    //xTaskCreate(IRSens_1, "IRSens1", 128, NULL, 2, NULL);
+    //xTaskCreate(Switch_0, "SWITCH0", 128, NULL, 3, NULL);
+    //xTaskCreate(Switch_1, "SWITCH1", 128, NULL, 3, NULL);
+    xTaskCreate(IRSensors, "IRSENSORS", 256, NULL, 2, NULL);
+    xTaskCreate(IRDistance, "IRDISTANCE", 256, NULL, 2, NULL);
     xTaskCreate(StatusLED, "STATUSLED", 128, NULL, 1, NULL);
-    //xTaskCreate(WiFi, "WIFI", 512, NULL, 2, NULL);
+
     vTaskStartScheduler();	//start the freertos scheduler
 
 	return 0;				//should not be reached!
@@ -95,61 +102,67 @@ static void Switch_0(void *pvParameters)
 
 
 
-static void WiFi(void *pvParameters)
+static void ConfigWiFi()
 {
   trace_init();
 
   wifi_init();
-
-  while (1)
-  {
-  }
 }
 
-static void ConfigUART()
-{
-  uart_init();
-}
-
-
-static void IRSens_0(void *pvParameters)
+static void IRSensors(void *pvParameters)
 {
   trace_init();
   ft_init();
 
-  //vTaskDelay(1);
+  digital_configure_pin(IRSENSOR0, DD_CFG_INPUT_PULLUP);
+  digital_configure_pin(IRSENSOR1, DD_CFG_INPUT_PULLUP);
 
-  uint16_t IRSense0;
-
-  digital_configure_pin(DD_PIN_PF10, DD_CFG_INPUT_PULLDOWN);
-  //digital_set_pin(DD_PIN_PF10, DD_LEVEL_LOW);
+  uint16_t IRSensor0;
+  uint16_t IRSensor1;
+  char data[14];
 
   while (1)
   {
-    ft_start_sampling(DD_PIN_PF10);
+    ft_start_sampling(IRSENSOR0);
 
-    while(!ft_is_sampling_finished(DD_PIN_PF10))
+    while(!ft_is_sampling_finished())
     {
       vTaskDelay(1);
     }
 
-    //ft_set_sampling_finished_handler();
-    IRSense0 = ft_get_transform(DFT_FREQ100);
+    IRSensor0 = ft_get_transform(DFT_FREQ100);
+
+    ft_start_sampling(IRSENSOR1);
+
+    while(!ft_is_sampling_finished())
+    {
+      vTaskDelay(1);
+    }
+
+    IRSensor1 = ft_get_transform(DFT_FREQ100);
+
+    //sprintf(data,"%" PRIu16 "_%" PRIu16, IRSensor0, IRSensor1);
+    //traces(data);
   }
 }
 
-/*static void IRSens_1(void *pvParameters)
+static void IRDistance(void *pvParameters)
 {
   trace_init();
+  adc_init();
 
-  digital_configure_pin(DD_PIN_PF9, DD_CFG_INPUT_PULLDOWN);
-  digital_set_pin(DD_PIN_PF9, DD_LEVEL_LOW);
+  uint32_t IRDistance0;
+
+  char data[11];
 
   while (1)
   {
+    vTaskDelay(10);
 
+    IRDistance0 = adc_get_value(IRDISTANCE0);
 
+    sprintf(data,"%" PRIu32, IRDistance0);
+    traces(data);
   }
-}*/
-
+}
 
